b0fe923c0931631762c3fc37c64f48b1ca8d658f
B Kleinen <kleinen@infrastructure.de>
2013-11-02 11:12:30 +0100

    initial commit

---
+++ gilded_rose_spec.rb
@@ -0,0 +1,82 @@
+require 'json'
+require 'simplecov'
+SimpleCov.start
+
+require 'rspec/given'
+require 'gilded_rose'
+
+describe "#update_quality" do
+
+  context "with a single" do
+    Given(:initial_sell_in) { 5 }
+    Given(:initial_quality) { 10 }
+    Given(:item) { Item.new(name, initial_sell_in, initial_quality) }
+
+    When { update_quality([item]) }
+
+    context "normal item" do
+      Given(:name) { "NORMAL ITEM" }
+
+      Invariant { item.sell_in.should == initial_sell_in-1 }
+
+      context "before sell date" do
+        Then { item.quality.should == initial_quality-1 }
+      end
+
+      context "after sell date" do
+        Given(:initial_sell_in) { -10 }
+        Then { item.quality.should == initial_quality-2 }
+      end
+
+    end
+
+    context "Aged Brie" do
+      Given(:name) { "Aged Brie" }
+
+      Invariant { item.sell_in.should == initial_sell_in-1 }
+
+      context "before sell date" do
+        Then { item.quality.should == initial_quality+1 }
+
+        context "with max quality" do
+          Given(:initial_quality) { 50 }
+          Then { item.quality.should == initial_quality }
+        end
+      end
+    end
+
+    context "conjured item" do
+      before { pending }
+      Given(:name) { "Conjured Mana Cake" }
+
+      Invariant { item.sell_in.should == initial_sell_in-1 }
+
+      context "before the sell date" do
+        Given(:initial_sell_in) { 5 }
+        Then { item.quality.should == initial_quality-2 }
+
+        context "at zero quality" do
+          Given(:initial_quality) { 0 }
+          Then { item.quality.should == initial_quality }
+        end
+      end
+    end
+  end
+
+  context "with several objects" do
+    Given(:items) {
+      [
+        Item.new("NORMAL ITEM", 5, 10),
+        Item.new("Aged Brie", 3, 10),
+      ]
+    }
+
+    When { update_quality(items) }
+
+    Then { items[0].quality.should == 9 }
+    Then { items[0].sell_in.should == 4 }
+
+    Then { items[1].quality.should == 11 }
+    Then { items[1].sell_in.should == 2 }
+  end
+end
